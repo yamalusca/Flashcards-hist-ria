f17264b183926a9e9756bab0b8ee53e52091b967
Lucas Alfred Yamafuku <148967417+yamalusca@users.noreply.github.com>
2024-09-27 17:12:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'HISTÓRIA',
+    'O que foi a revolução francesa?',
+    'Foi a revolta dos camponeses que derrubou a monarquia francesa'
+)
+
+criaCartao(
+    'HISTÓRIA',
+    'Qual o nome da Santa que liderou o exército francês e morreu pela própria igreja?',
+    'Joana Darc'
+)
+
+criaCartao(
+    'HISTÓRIA',
+    'O que foi o tratado de Tordesilhas?',
+    'Foi o acordo que dividiu a terra entre Portugal e Espanha'
+)
+
+criaCartao(
+    'HISTÓRIA',
+    'Quem eram os principais "aliados" na segunda guerra  mundial?',
+    'EUA, União Soviética, Reino Unido e França'
+)
